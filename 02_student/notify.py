3163eb8271d13a1618c43ca46fe3269540dc7a55
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-08-06 18:47:22 +0500

    Multi Parameters

--- 02_student/notify.py
+++ 02_student/notify.py
@@ -8,3 +8,9 @@ def notification(filter:str):
 
 # http://127.0.0.1:8000/notifications?filter=14 August Holiday
 # Put ? to give parameters
+
+@app.get("/notifications/")
+def notification_id(filter:str, id:int, name:str):
+    return {"message": f"Notification: {filter}, ID: {id}, Name: {name}"}
+
+#http://127.0.0.1:8000/notifications/?filter=14%20August%20Holiday&id=7&name=Abdul%20Khaliq
