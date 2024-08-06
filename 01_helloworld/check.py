954a540d2544a00d3085e2802a26181b35e622f6
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-08-06 18:07:10 +0500

    Greet Check

--- 01_helloworld/check.py
+++ 01_helloworld/check.py
@@ -5,3 +5,11 @@ print("Ststus Code: ",response.status_code)
 print("Message: ", response.text)
 print("JSON: ", response.json())
 print("Header: ", response.headers)
+
+print("\n")
+
+response = requests.get("http://127.0.0.1:8000/ak")
+print("Ststus Code: ",response.status_code)
+print("Message: ", response.text)
+print("JSON: ", response.json())
+print("Header: ", response.headers)
