647fad2ed811dd7bf744b334cb8dbafc699210af
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2026-03-31 09:25:07 +0500

    Get Latest Post

--- 04_plain_social_app/app/main.py
+++ 04_plain_social_app/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+class Post(BaseModel):
+    title: str
+    content: str
+    Published: bool = True
 
 @app.get("/")
 def read_root():
