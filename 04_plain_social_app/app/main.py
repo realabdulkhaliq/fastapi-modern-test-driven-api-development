bc70d9a193f22be20d4e663e9b8262fb86f909fb
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2026-04-01 09:28:42 +0500

    Create  Post

--- 04_plain_social_app/app/main.py
+++ 04_plain_social_app/app/main.py
@@ -11,6 +11,10 @@ class Post(BaseModel):
 def read_root():
     return {"message": "Hello World"}
 
+@app.post("/createpost")
+def create_post(post: Post):
+    print(post)
+
 @app.get("/posts")
 def get_posts():
     return {"All Posts": "These are all the posts"}
