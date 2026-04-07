2813a0f158d24f42ef69422f1f924d9beb7f6f66
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2026-04-07 10:53:23 +0500

    Search Post by ID

--- 04_plain_social_app/app/main.py
+++ 04_plain_social_app/app/main.py
@@ -35,4 +35,7 @@ def get_latest_posts():
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-
+    post = next((post for post in my_posts if post["id"] == id), None)
+    if post is None:
+        return {"message": f"Post with id {id} not found"}
+    return {"post": post}
