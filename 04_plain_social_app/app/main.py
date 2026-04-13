f400f6acb5ac25dc815249d7c4118bdc14892f6d
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2026-04-13 11:16:36 +0500

    Update  Post by ID

--- 04_plain_social_app/app/main.py
+++ 04_plain_social_app/app/main.py
@@ -3,10 +3,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-my_posts = []
-# my_posts = [{"title": "Post 1", "content": "Content of post 1", "published": True, "id": 1},
-#             {"title": "Post 2", "content": "Content of post 2", "published": False, "id": 2},
-#             {"title": "Post 3", "content": "Content of post 3", "published": True, "id": 3}]
+my_posts = [{"title": "Post 1", "content": "Content of post 1", "published": True, "id": 1},
+            {"title": "Post 2", "content": "Content of post 2", "published": False, "id": 2},
+            {"title": "Post 3", "content": "Content of post 3", "published": True, "id": 3}]
 
 def find_post(id):
     for post in my_posts:
